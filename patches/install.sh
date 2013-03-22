14380c47fcb8d7f1b0b34c0b803cc1f301a160be
TheWhisp <daavis.90@gmail.com>
2013-03-22 18:40:09 +0200

    common: fix typo in patch installer path

--- patches/install.sh
+++ patches/install.sh
@@ -3,5 +3,5 @@ rootdirectory="$PWD"
 cd $rootdirectory
 cd build
 echo "I: Applying build patches..."
-git am $rootdirectory/device/samsung/msm7x27-common/patches/android_build/*.patch
+git am $rootdirectory/device/samsung/msm7x27a-common/patches/android_build/*.patch
 cd $rootdirectory
