1d2a9ee3f945abdb674ecd5faf0e5f099ba91759
Dāvis Mālnieks <daavis.90@gmail.com>
2013-06-11 19:09:22 +0300

    add camera HAL
    for testing purposes, taken from androidarmv6 msm7x27 devices
    
    Change-Id: I1fb9ff394db34874444f6bb5f789ccce37c9cac5

---
+++ camera/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS           += -O3
+LOCAL_MODULE_PATH      := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE           := camera.$(TARGET_BOARD_PLATFORM)
+
+LOCAL_MODULE_TAGS      := optional
+
+
+LOCAL_SRC_FILES        := cameraHAL.cpp
+LOCAL_C_INCLUDES       := $(TOP)/frameworks/base/include
+LOCAL_C_INCLUDES       += hardware/qcom/display-legacy/libgralloc
+
+LOCAL_SHARED_LIBRARIES := liblog libutils libcutils
+LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
+
+LOCAL_LDFLAGS          += -Lvendor/samsung/msm7x27a-common/proprietary/lib -lcamera
+
+include $(BUILD_SHARED_LIBRARY)
