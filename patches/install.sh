4a8aa9c0c55c386c3484f9ffe91871d6f5c55050
TheWhisp <daavis.90@gmail.com>
2013-08-18 12:40:32 +0300

    update patches

--- patches/install.sh
+++ patches/install.sh
@@ -9,3 +9,8 @@ cd frameworks/native
 echo "Applying frameworks/native patches..."
 git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_native/*.patch
 cd $rootdirectory
+
+## Cherry-picks
+cd dalvik
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_dalvik refs/changes/56/47756/1 && git cherry-pick FETCH_HEAD
+cd $rootdirectory
