b7c4d29b8d9859eeb83841d6d70db71183e57c93
TheWhisp <daavis.90@gmail.com>
2013-08-24 20:55:50 +0300

    wifi: switch back to compat3.9rc4.2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,19 +87,24 @@ BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_ath6kl
 WIFI_EXT_MODULE_NAME := cfg80211
 WIFI_EXT_MODULE_PATH := /system/lib/modules/cfg80211.ko
+WIFI_AP_DRIVER_MODULE_ARG := "suspend_mode=3 wow_mode=2 ath6kl_p2p=1 recovery_enable=1"
 WIFI_AP_DRIVER_MODULE_NAME := ath6kl
 WIFI_AP_DRIVER_MODULE_PATH := /system/lib/modules/ath6kl.ko
+WIFI_DRIVER_MODULE_ARG := "suspend_mode=3 wow_mode=2 ath6kl_p2p=1 recovery_enable=1"
 WIFI_DRIVER_MODULE_NAME := ath6kl
 WIFI_DRIVER_MODULE_PATH := /system/lib/modules/ath6kl.ko
 
-KERNEL_WIFI_MODULES:
-	$(MAKE) -C hardware/atheros/backports-wireless defconfig-ath6kl
-	export CROSS_COMPILE=$(ARM_EABI_TOOLCHAIN)/arm-eabi-; $(MAKE) -C hardware/atheros/backports-wireless KLIB=$(KERNEL_SRC) KLIB_BUILD=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE)
-	cp `find hardware/atheros/backports-wireless -name *.ko` $(KERNEL_MODULES_OUT)/
-	arm-eabi-strip --strip-debug `find $(KERNEL_MODULES_OUT) -name *.ko`
-	$(MAKE) -C hardware/atheros/backports-wireless clean
-
-TARGET_KERNEL_MODULES := KERNEL_WIFI_MODULES
+KERNEL_EXTERNAL_MODULES:
+	## Wipe & prepare ath6kl-compat working directory
+	rm -rf $(OUT)/ath6kl-compat
+	cp -a hardware/atheros/ath6kl-compat $(OUT)/
+	## Run build
+	$(MAKE) -C $(OUT)/ath6kl-compat KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE)
+	## Copy & strip modules (to economize space)
+	$(TARGET_OBJCOPY) --strip-unneeded $(OUT)/ath6kl-compat/compat/compat.ko $(KERNEL_MODULES_OUT)/compat.ko
+	$(TARGET_OBJCOPY) --strip-unneeded $(OUT)/ath6kl-compat/drivers/net/wireless/ath/ath6kl/ath6kl.ko $(KERNEL_MODULES_OUT)/ath6kl.ko
+	$(TARGET_OBJCOPY) --strip-unneeded $(OUT)/ath6kl-compat/net/wireless/cfg80211.ko $(KERNEL_MODULES_OUT)/cfg80211.ko
+TARGET_KERNEL_MODULES := KERNEL_EXTERNAL_MODULES
 
 ## RIL
 BOARD_USES_LEGACY_RIL := true
