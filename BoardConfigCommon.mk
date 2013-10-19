d59808f8893570249351fe2b076d1a1486ea6439
TheWhisp <daavis.90@gmail.com>
2013-10-19 11:04:54 +0300

    charger: use AOSP solution

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -122,6 +122,11 @@ BOARD_USE_LEGACY_TOUCHSCREEN := true
 ## Samsung has weird framebuffer
 TARGET_NO_INITLOGO := true
 
+## LPM charging
+BOARD_LPM_BOOT_ARGUMENT_NAME := androidboot.boot_pause
+BOARD_LPM_BOOT_ARGUMENT_VALUE := batt
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
 ## Bootanimation
 TARGET_BOOTANIMATION_USE_RGB565 := true
 
--- msm7x27a.mk
+++ msm7x27a.mk
@@ -53,6 +53,11 @@ PRODUCT_PACKAGES += \
     libqcomfm_jni \
     FM2
 
+## Charger
+PRODUCT_PACKAGES += \
+    charger \
+    charger_res_images
+
 ## Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
