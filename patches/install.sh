363a3ff1c483140a400c68335b9569f4c1264176
TheWhisp <daavis.90@gmail.com>
2013-11-02 18:57:17 +0200

    common: experimental: build with 3.4.x kernel
    * Enable ION
    * Disable ARM PIE and legacy camera hacks
    * Add patch to enable Qualcomm enhanced audio and LPA

--- patches/install.sh
+++ patches/install.sh
@@ -9,6 +9,10 @@ cd frameworks/native
 echo "Applying frameworks/native patches..."
 git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_native/*.patch
 cd $rootdirectory
+cd frameworks/av
+echo "Applying frameworks/av patches..."
+git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_av/*.patch
+cd $rootdirectory
 cd system/core
 echo "Applying system/core patches..."
 git am $rootdirectory/device/samsung/msm7x27a-common/patches/system_core/*.patch
