f879198a28915bf1bd48e136623c3618f784ad01
TheWhisp <daavis.90@gmail.com>
2013-11-10 13:53:33 +0200

    patches: update system/core patches

--- patches/install.sh
+++ patches/install.sh
@@ -9,10 +9,10 @@ cd $rootdirectory
 #echo "Applying frameworks/av patches..."
 #git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_av/*.patch
 #cd $rootdirectory
-#cd system/core
-#echo "Applying system/core patches..."
-#git am $rootdirectory/device/samsung/msm7x27a-common/patches/system_core/*.patch
-#cd $rootdirectory
+cd system/core
+echo "Applying system/core patches..."
+git am $rootdirectory/device/samsung/msm7x27a-common/patches/system_core/*.patch
+cd $rootdirectory
 cd hardware/libhardware
 echo "Applying hardware/libhardware patches..."
 git am $rootdirectory/device/samsung/msm7x27a-common/patches/hardware_libhardware/*.patch
