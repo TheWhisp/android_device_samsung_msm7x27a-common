a89f44d790abcf13b3cab498072786ea71fcb676
TheWhisp <daavis.90@gmail.com>
2013-11-10 18:27:59 +0200

    patches: support for FM

--- patches/install.sh
+++ patches/install.sh
@@ -5,10 +5,10 @@ cd $rootdirectory
 #echo "Applying frameworks/base patches..."
 #git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_base/*.patch
 #cd $rootdirectory
-#cd frameworks/av
-#echo "Applying frameworks/av patches..."
-#git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_av/*.patch
-#cd $rootdirectory
+cd frameworks/av
+echo "Applying frameworks/av patches..."
+git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_av/0*.patch
+cd $rootdirectory
 cd system/core
 echo "Applying system/core patches..."
 git am $rootdirectory/device/samsung/msm7x27a-common/patches/system_core/*.patch
@@ -16,4 +16,8 @@ cd $rootdirectory
 cd hardware/libhardware
 echo "Applying hardware/libhardware patches..."
 git am $rootdirectory/device/samsung/msm7x27a-common/patches/hardware_libhardware/*.patch
+cd $rootdirectory
+cd hardware/libhardware_legacy
+echo "Applying hardware/libhardware_legacy patches..."
+git am $rootdirectory/device/samsung/msm7x27a-common/patches/hardware_libhardware_legacy/*.patch
 cd $rootdirectory
