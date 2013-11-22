cee5ae7e6c0932e1bd69680fa4e96e9db58a4f41
TheWhisp <daavis.90@gmail.com>
2013-11-22 16:21:10 +0200

    patches: clean-up, use new apply system (kanged from Dazzozo)

--- patches/install.sh
+++ patches/install.sh
@@ -1,23 +1,17 @@
-echo "Obtaining build directory..."
-rootdirectory="$PWD"
-cd $rootdirectory
-#cd frameworks/base
-#echo "Applying frameworks/base patches..."
-#git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_base/*.patch
-#cd $rootdirectory
-cd frameworks/av
-echo "Applying frameworks/av patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_av/0*.patch
-cd $rootdirectory
-cd system/core
-echo "Applying system/core patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/system_core/*.patch
-cd $rootdirectory
-cd hardware/libhardware
-echo "Applying hardware/libhardware patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/hardware_libhardware/*.patch
-cd $rootdirectory
-cd hardware/libhardware_legacy
-echo "Applying hardware/libhardware_legacy patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/hardware_libhardware_legacy/*.patch
-cd $rootdirectory
+#!/bin/sh
+
+MYABSPATH=$(readlink -f "$0")
+PATCHBASE=$(dirname "$MYABSPATH")
+CMBASE=$(readlink -f "$PATCHBASE/../../../../")
+
+for i in $(find "$PATCHBASE"/* -type d); do
+        PATCHNAME=$(basename "$i")
+        PATCHTARGET=$PATCHNAME
+        for i in $(seq 4); do
+                PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
+                if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
+        done
+        echo applying $PATCHNAME to $PATCHTARGET
+        cd "$CMBASE/$PATCHTARGET" || exit 1
+        git am -3 "$PATCHBASE/$PATCHNAME"/* || exit 1
+done
