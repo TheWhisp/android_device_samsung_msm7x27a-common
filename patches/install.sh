99de85670f919578966ee449bc82d9eb3c6b8a58
TheWhisp <daavis.90@gmail.com>
2013-12-03 20:31:26 +0200

    patches: remove wpa_supplicant delay patch, fork install script from Dazzozo

--- patches/install.sh
+++ patches/install.sh
@@ -1,39 +1,17 @@
-echo "Obtaining build directory..."
-rootdirectory="$PWD"
-cd $rootdirectory
-cd frameworks/base
-echo "Applying frameworks/base patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_base/*.patch
-cd $rootdirectory
-cd frameworks/native
-echo "Applying frameworks/native patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_native/*.patch
-cd $rootdirectory
-cd frameworks/av
-echo "Applying frameworks/av patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/frameworks_av/*.patch
-cd $rootdirectory
-cd system/core
-echo "Applying system/core patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/system_core/*.patch
-cd $rootdirectory
-cd system/vold
-echo "Applying system/vold patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/system_vold/*.patch
-cd $rootdirectory
-cd external/wpa_supplicant_8
-echo "Applying external/wpa_supplicant_8 patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/external_wpa_supplicant_8/*.patch
-cd $rootdirectory
-cd packages/apps/Settings
-echo "Applying packages/apps/Settings patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/packages_apps_Settings/*.patch
-cd $rootdirectory
-cd hardware/libhardware
-echo "Applying hardware/libhardware patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/hardware_libhardware/*.patch
-cd $rootdirectory
-cd vendor/tmobile/apps/ThemeChooser
-echo "Applying vendor/tmobile/apps/ThemeChooser patches..."
-git am $rootdirectory/device/samsung/msm7x27a-common/patches/vendor_tmobile_apps_ThemeChooser/*.patch
-cd $rootdirectory
+#!/bin/sh
+
+MYABSPATH=$(readlink -f "$0")
+PATCHBASE=$(dirname "$MYABSPATH")
+CMBASE=$(readlink -f "$PATCHBASE/../../../../")
+
+for i in $(find "$PATCHBASE"/* -type d); do
+        PATCHNAME=$(basename "$i")
+        PATCHTARGET=$PATCHNAME
+        for i in $(seq 4); do
+                PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
+                if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
+        done
+        echo applying $PATCHNAME to $PATCHTARGET
+        cd "$CMBASE/$PATCHTARGET" || exit 1
+        git am -3 "$PATCHBASE/$PATCHNAME"/* || exit 1
+done
