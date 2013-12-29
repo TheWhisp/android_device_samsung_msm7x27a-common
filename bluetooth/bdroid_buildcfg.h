f17ca8258c4e01869385c4af004f609705b67720
TheWhisp <daavis.90@gmail.com>
2013-12-29 11:46:54 +0200

    bluetooth: attempt to fix BT tethering

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,11 +18,13 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "msm7x27a"
+#define BTM_DEF_LOCAL_NAME   "Samsung MSM7x27A"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #define BLE_INCLUDED FALSE
 #define BTA_GATT_INCLUDED FALSE
 #define SMP_INCLUDED FALSE
+
+#define BTM_BYPASS_EXTRA_ACL_SETUP TRUE
 
 #endif
