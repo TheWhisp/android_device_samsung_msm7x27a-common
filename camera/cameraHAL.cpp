94bc2a95aefb42b73811a7a0b94ad7d2ca604790
TheWhisp <daavis.90@gmail.com>
2014-02-08 23:02:11 +0200

    update camera hal and media profiles

--- camera/cameraHAL.cpp
+++ camera/cameraHAL.cpp
@@ -215,20 +215,25 @@ static void wrap_data_callback_timestamp(nsecs_t timestamp, int32_t msg_type, co
 
 void CameraHAL_FixupParams(android::CameraParameters &camParams) {
     const char *video_sizes = "640x480,384x288,352x288,320x240,240x160,176x144";
-    const char *preferred_video_size = "640x480";
-    const char *preferred_size = "640x480";
+    const char *preferred_size = "480x320";
 
     camParams.set(CameraParameters::KEY_VIDEO_FRAME_FORMAT, CameraParameters::PIXEL_FORMAT_YUV420SP);
 
-    if (!camParams.get(CameraParameters::KEY_VIDEO_SIZE)) {
-	 camParams.set(CameraParameters::KEY_VIDEO_SIZE, preferred_video_size);
-    }
+    camParams.set(CameraParameters::KEY_PREFERRED_PREVIEW_SIZE_FOR_VIDEO,  preferred_size);
 
+#if 0
     if (!camParams.get(CameraParameters::KEY_SUPPORTED_VIDEO_SIZES)) {
-         camParams.set(CameraParameters::KEY_SUPPORTED_VIDEO_SIZES, video_sizes); }
+         camParams.set(CameraParameters::KEY_SUPPORTED_VIDEO_SIZES, video_sizes);
+    }
+#endif
 
     if (!camParams.get(CameraParameters::KEY_MAX_NUM_FOCUS_AREAS)) {
-        camParams.set(CameraParameters::KEY_MAX_NUM_FOCUS_AREAS, 1); }
+        camParams.set(CameraParameters::KEY_MAX_NUM_FOCUS_AREAS, 1);
+    }
+
+    if (!camParams.get(CameraParameters::KEY_VIDEO_SIZE)) {
+         camParams.set(CameraParameters::KEY_VIDEO_SIZE, preferred_size);
+    }
 
     if (camParams.get(android::CameraParameters::KEY_MAX_CONTRAST)) {
       	camParams.set("max-contrast", camParams.get(android::CameraParameters::KEY_MAX_CONTRAST));
@@ -364,9 +369,11 @@ int camera_preview_enabled(struct camera_device * device) {
     return qCamera->previewEnabled();
 }
 
+#if 0
 int camera_store_meta_data_in_buffers(struct camera_device * device, int enable) {
     return 0;
 }
+#endif
 
 int camera_start_recording(struct camera_device * device) {
     return qCamera->startRecording();
@@ -510,7 +517,7 @@ int camera_device_open(const hw_module_t* module, const char* name, hw_device_t*
         camera_ops->start_preview              = camera_start_preview;
         camera_ops->stop_preview               = camera_stop_preview;
         camera_ops->preview_enabled            = camera_preview_enabled;
-        camera_ops->store_meta_data_in_buffers = camera_store_meta_data_in_buffers;
+        //camera_ops->store_meta_data_in_buffers = camera_store_meta_data_in_buffers;
         camera_ops->start_recording            = camera_start_recording;
         camera_ops->stop_recording             = camera_stop_recording;
         camera_ops->recording_enabled          = camera_recording_enabled;
